34e2b3ef46db5f3f7f682b663ddbd1a28b43ecad
Brian <qmfkdldks@gmail.com>
2016-08-02 19:51:11 -0300

    Poquer problem code has updated.

--- poquer/main.cpp
+++ poquer/main.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <queue>
 
-void startGame(std::string * manos, std::string * result);
+void startGame(int gameCount, std::string * manos);
 
 int main()
 {
@@ -13,12 +14,12 @@ int main()
     file.open("manos.in");
 
     int gameCount;
-    std::string *str;
+    std::string *allCards;
 
     if(file.is_open())
     {
         gameCount = std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n');
-        str = new std::string[gameCount];
+        allCards = new std::string[gameCount];
         file.seekg(0);
 
         for(int i = 0; i < gameCount; ++i)
@@ -27,38 +28,40 @@ int main()
             for(int j = 0; j < 20 && file.good(); ++j)
             {
                 file >> c;
-                str[i].push_back(c);
+                allCards[i].push_back(c);
             }
         }
 
         for(int i = 0; i < gameCount; ++i)
         {
-            std::cout << i << ":" << str[i] << std::endl;
+            std::cout << i << ":" << allCards[i] << std::endl;
         }
     }
+
+    startGame(gameCount, allCards);
 
     file.clear();
     file.close();
     return 0;
 }
 
-#define NSUITS 4
+#define NSHAPES 4
 #define NCARDS 52
 
 // Code section
 char values[] = "23456789JQKA";
-char suits[] = "CDHS";
+char shapes[] = "CDHS";
 
 int rank_card(char value, char suit)
 {
     int i,j;
 
 // Find the given card
-    for(i = 0; i < (NCARDS/NSUITS); ++i)
+    for(i = 0; i < (NCARDS/NSHAPES); ++i)
         if(values[i] == value)
-            for(j = 0; j < NSUITS; ++j)
-                if(suits[j] == suit)
-                    return (i * NSUITS + j);
+            for(j = 0; j < NSHAPES; ++j)
+                if(shapes[j] == suit)
+                    return (i * NSHAPES + j);
 
     /*return card number in 52 number of card
     after searching card number(12) and shape(4)
@@ -66,28 +69,173 @@ int rank_card(char value, char suit)
     so multiplying number of card with 4 gives 52*/
 }
 
+int convertToInt(char num)
+{
+    int pos = std::distance(values, std::find(values, values + 12, num));
+    return pos;
+}
+
 struct Card
 {
-    Card(char s, char n) : shape(s), number(n){ value = rank_card(shape, number); }
+    Card() {}
+    Card(char n, char s) : mShape(s), mNumber(n){ value = rank_card(mNumber, mShape); }
     int index() {
-        for(int i = 0; i < (NCARDS / NSUITS); ++i)
-            if(values[i] == number)
+        for(int i = 0; i < (NCARDS / NSHAPES); ++i)
+            if(values[i] == mNumber)
                 return i;
     }
-    char shape, number;
+
+
+    bool operator < (const Card& right) const
+    {
+        return convertToInt(mNumber) < convertToInt(right.mNumber);
+    }
+
+    char mShape, mNumber;
     int value;
 };
 
+typedef std::vector<Card> Cards;
+
+bool EscaleraDeColor(Cards inCards)
+{
+    int compares[5];
+    int seed = convertToInt(inCards[0].mNumber);
+    std::generate_n(compares, 5, [seed]()mutable{ return ++seed; });
+
+    bool isConsecutive = true;
+    for(int i = 0; i < 5; ++i)
+    {
+        std::cout  << "i: "<< inCards[i].mNumber << " ";
+        std::cout << compares[i] << std::endl;
+
+        if(compares[i] != inCards[i].mNumber)
+        {
+            isConsecutive = false;
+            break;
+        }
+    }
+
+    Card init = inCards[0];
+    int iqualCount = std::count_if(inCards.begin(), inCards.end(), [init](Card c){ return (init.mShape == c.mShape);});
+
+    std::cout << iqualCount << std::endl;
+
+    return ( isConsecutive && iqualCount == 5);
+}
+
+bool poquer(Cards inCards, int & v)
+{
+    int maxCount = 0;
+    for(int i = 0; i < 5; ++i)
+    {
+        Card init = inCards[i];
+        int iqualCount = std::count_if(inCards.begin(), inCards.end(), [init](Card c){ return (init.mNumber == c.mNumber);});
+
+        if(maxCount < iqualCount)
+        {
+           maxCount = iqualCount;
+           v = convertToInt(init.mNumber);
+        }
+    }
+
+    return maxCount == 4;
+}
+
+bool full(Cards inCards, int & v)
+{
+    int trioValue = 0;
+    int maxCount = 0;
+    for(int i = 0; i < 5; ++i)
+    {
+        Card init = inCards[i];
+        int iqualCount = std::count_if(inCards.begin(), inCards.end(), [init](Card c){ return (init.mNumber == c.mNumber);});
+
+        if(maxCount < iqualCount)
+        {
+            maxCount = iqualCount;
+            trioValue = init.mNumber;
+            v = trioValue;
+        }
+    }
+
+    // if it's TRIO
+    if(maxCount == 3)
+    {
+        for(int i = 0; i < 5; ++i)
+        {
+            Card init = inCards[i];
+
+            if(init.mNumber != trioValue){
+                int iqualCount = std::count_if(inCards.begin(), inCards.end(), [init](Card c)
+                                           {
+                                               return (init.mNumber == c.mNumber);
+                                           });
+                return (iqualCount == 2);
+            }
+        }
+    }
+    else
+        return false;
+}
+
 struct Player
 {
-    Player(std::string cartas) {
+    Player(std::string name, std::string cartas) : mName(name) {
+        std::cout << "Cards:" << cartas << std::endl;
+        for(int i = 0; i < cartas.size(); i = i + 2)
+        {
+            Card c(cartas[i], cartas[i + 1]);
+            mCards.push_back(c);
+        }
+
+        std::sort(mCards.begin(), mCards.end());
+    }
+
+    int v = 0;
+    int calculate()
+    {
+        if(EscaleraDeColor(mCards))
+        {
+            mPoint = 9;
+            mHighestCard = *(mCards.end() - 1);
+        }
+        else if(poquer(mCards, v))
+        {
+            mPoint = 8;
+        }
+        else if(full(mCards, v))
+        {
+
+        }
 
     }
+
+    int getPoint()
+    {
+        return mPoint;
+    }
+
+    int getHighestCard()
+    {
+        return convertToInt(mHighestCard.mNumber);
+    }
+
+    std::string mName;
+    std::vector<Card> mCards;
+    int mPoint;
+    Card mHighestCard;
 };
 
 
 
-void startGame(std::string * manos, std::string * result)
+void startGame(int gameCount, std::string * manos)
 {
+    for(int i = 0; i < gameCount; ++i)
+    {
+        Player("Black", std::string(manos[i], 0, 10 ) );
+        Player("White", std::string(manos[i], 10, 10) );
+    }
+
 
 }
